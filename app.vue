9837d5ce762204e3923ce9e382dbbe16cd6d2188
Cyrus Collier <ccollier@situationinteractive.com>
2022-05-19 17:54:13 -0500

    Add TailwindCSS and HeadlessUI

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <!-- Remove this component to get started! -->
-    <NuxtWelcome />
+    <TabGroup>
+      <TabList class="inline-flex gap-4 border mb-4">
+        <Tab v-slot="{ selected }" as="template"><button class="p-4" :class="selected ? 'bg-gray-300' : 'bg-gray-100'">Tab 1</button></Tab>
+        <Tab v-slot="{ selected }" as="template"><button class="p-4" :class="selected ? 'bg-gray-300' : 'bg-gray-100'">Tab 2</button></Tab>
+      </TabList>
+      <TabPanels class="border p-4">
+        <TabPanel>
+          This is the content of the first tab panel.
+        </TabPanel>
+        <TabPanel>
+          This is the content of the second tab panel.
+        </TabPanel>
+      </TabPanels>
+    </TabGroup>
   </div>
 </template>
+<script>
+import {Tab, TabGroup, TabList, TabPanel, TabPanels} from "@headlessui/vue";
+export default {
+  components: {TabPanel, TabPanels, Tab, TabList, TabGroup}
+}
+</script>
